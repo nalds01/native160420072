ec135c00d5c017f1a237b262282a2044143febf4
nalds01 <s160420072@student.ubaya.ac.id>
2023-03-01 14:28:12 +0700

    login

--- app/src/main/java/id/ac/ubaya/informatika/native160420072/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/native160420072/MainActivity.kt
@@ -7,8 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // test
-        // diva
-        //opo sehs
+// login bos
     }
 }
